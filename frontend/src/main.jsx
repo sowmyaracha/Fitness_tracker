0d0b4ec9d75796afb66281ffdec1d10c8067fc82
Sowmya Racha <sowmyaracha1999@gmail.com>
2026-05-08 13:40:19 -0500

    Replace all transporter.sendMail with resend.emails.send

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,5 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import axios from "axios";
+
+axios.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
 
 import App from "./App.jsx";
 
