4091d33c9ee45b19db3de1b8d22bfff31db83334
Sowmya Racha <sowmyaracha1999@gmail.com>
2026-05-08 11:38:21 -0500

    Modified mailController Host

--- backend/controllers/mailController.js
+++ backend/controllers/mailController.js
@@ -9,7 +9,9 @@ import {
 
 // Nodemailer transporter setup
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
