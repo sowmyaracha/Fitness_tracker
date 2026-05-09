d7ed0f5f73fa6a3eb53822d2f32cd1fff22a43f3
Sowmya Racha <sowmyaracha1999@gmail.com>
2026-05-08 20:01:31 -0500

    Simplify main.jsx remove duplicate interceptor

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import axios from "./utils/axiosInstance";
 import App from "./App.jsx";
-
-axios.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-});
 
 createRoot(document.getElementById("root")).render(
     <App />
