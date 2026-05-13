0af0a9473701d335349cdc97da2e06ae725d499a
Sowmya Racha <sowmyaracha1999@gmail.com>
2026-05-13 01:10:35 -0500

    Add profile completion banner on dashboard

--- backend/index.js
+++ backend/index.js
@@ -36,6 +36,11 @@ app.use("/api/user", userRouter);
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
+// Keep alive ping
+setInterval(() => {
+  console.log("Keep alive ping");
+}, 1000 * 60 * 14); // ping every 14 minutes
+
 app.listen(3000, () => {
   console.log("Server is running on http://localhost:3000");
 });
